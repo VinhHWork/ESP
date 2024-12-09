b1ffdeda5133ac82296dd5114aad74e42e1e05b5
VinhHWork <156438417+VinhHWork@users.noreply.github.com>
2024-12-09 08:57:05 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ bool lightIsOn = false;
 unsigned long fanStartTime = 0;
 unsigned long motionDetectedTime = 0;
 unsigned long lightOnTime = 0;
-unsigned long lastMotionTime = 0;
+int prevMotionState = LOW;
 
 void setup() {
   Serial.begin(115200);  // Initialize serial communication at 115200 bps
@@ -30,50 +30,61 @@ void setup() {
   Serial.println("Setup complete.");
 }
 
-void loop() {
+void loop() { 
   int motionState = digitalRead(motionPin);
 
-  if (motionState == HIGH && !fanIsRunning) {
-    // Motion detected and fan is not running, turn on fan and light
-    Serial.println("Motion detected, turning on fan and light");
-    ledcWrite(0, 128);  // Set fan speed to 50%
-    digitalWrite(lightPin, HIGH);  // Turn on 5V LED
-    fanIsRunning = true;
-    lightIsOn = true;
-    fanStartTime = millis();
-    motionDetectedTime = millis();
-    lightOnTime = millis();
-    lastMotionTime = millis();
+  if (motionState == HIGH){
+    if (!lightIsOn) {
+      // Motion detected and light is not on, turn on light
+      Serial.println("Motion detected, turning on light");
+      digitalWrite(lightPin, HIGH);
+      lightIsOn = true;
+      lightOnTime = millis();
+    }
+
+    if (!fanIsRunning) {
+      // Motion detected and fan is not running, turn on fan and light
+      Serial.println("Motion detected, turning on fan");
+      ledcWrite(0, 128);  // Set fan speed to 50%
+      fanIsRunning = true;
+      fanStartTime = millis();
+    }
+    // Reset motion detected time
+    if (motionState != prevMotionState) {
+      motionDetectedTime = millis();
+    }
+    // Store previous motion state
+    prevMotionState = motionState;
   }
 
   if (motionState == LOW && lightIsOn) {
-    // No motion detected and light is on, turn off light
-    Serial.println("No motion detected, turning off light");
-    digitalWrite(lightPin, LOW);
-    lightIsOn = false;
+    if ((millis() - lightOnTime) >= 300000) {
+      // Adds a delay of 5 minutes before turning off the light incases where object is not moving
+      Serial.println("Light has been on for 5 minutes, turning it off");
+      digitalWrite(lightPin, LOW);
+      lightIsOn = false;
+    }
   }
+  if (fanIsRunning) {
 
-  if (fanIsRunning && (millis() - motionDetectedTime) < 300000) {
-    // Fan is running and motion was detected less than 5 minutes ago, increase fan speed
-    int fanSpeed = map(millis() - motionDetectedTime, 0, 300000, 128, 255);
-    Serial.print("Motion detected ");
-    Serial.print(millis() - motionDetectedTime);
-    Serial.print(" milliseconds ago, increasing fan speed to ");
-    Serial.println(fanSpeed);
-    ledcWrite(0, fanSpeed);
-  }
-
-  if (fanIsRunning && (millis() - fanStartTime) >= 300000) {
-    // Fan has been running for 5 minutes, set fan speed to 100%
-    Serial.println("Fan has been running for 5 minutes, setting fan speed to 100%");
-    ledcWrite(0, 255);
-  }
-
-  if (fanIsRunning && (millis() - fanStartTime) >= 7200000) {
-    // Fan has been running for 2 hours, turn it off
-    Serial.println("Fan has been running for 2 hours, turning it off");
-    ledcWrite(0, 0);
-    fanIsRunning = false;
+    if ((millis() - motionDetectedTime) < 300000){
+      // Fan is running and motion was detected less than 5 minutes ago, increase fan speed
+      int fanSpeed = map(millis() - motionDetectedTime, 0, 300000, 128, 255);
+      Serial.print(millis() - motionDetectedTime);
+      Serial.println(fanSpeed);
+      ledcWrite(0, fanSpeed);
+    }
+    if ((millis() - fanStartTime) >= 300000) {
+      // Fan has been running for 5 minutes, set fan speed to 100%
+      Serial.println("Fan has been running for 5 minutes, setting fan speed to 100%");
+      ledcWrite(0, 255);
+    }
+    if ((millis() - fanStartTime) >= 7200000) {
+      // Fan has been running for 2 hours, turn it off
+      Serial.println("Fan has been running for 2 hours, turning it off");
+      ledcWrite(0, 0);
+      fanIsRunning = false;
+    }
   }
 
   delay(100);
